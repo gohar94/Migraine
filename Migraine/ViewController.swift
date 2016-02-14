2818a43a4c70bd630034817c2e572f3494b01c5a
gohar94 <goharirfan94@gmail.com>
2016-02-14 10:33:30 +0500

    Initial Commit

---
+++ Migraine/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Migraine
+//
+//  Created by Gohar Irfan on 2/14/16.
+//  Copyright © 2016 Gohar Irfan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
