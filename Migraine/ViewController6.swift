dbb14f861a8e8570e01f76ff95a1a24d0c6dac88
gohar94 <goharirfan94@gmail.com>
2016-02-16 00:20:30 +0500

    first time info fields working

--- Migraine/ViewController6.swift
+++ Migraine/ViewController6.swift
@@ -8,10 +8,22 @@
 
 import UIKit
 
-class ViewController6: UIViewController {
+class ViewController6: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
     
     @IBOutlet var dateOfLMP: UITextField!
     @IBOutlet var dateOfNextPeriod: UITextField!
+    @IBOutlet weak var gender: UITextField!
+    @IBOutlet weak var birthControl: UITextField!
+    @IBOutlet weak var age: UITextField!
+    
+    let genderOptions = ["Female", "Male"]
+    let birthControlOptions = ["A", "B", "C"]
+    
+    enum PickerViewTag: Int {
+        // Integer values will be implicitly supplied; you could optionally set your own values
+        case PickerView1
+        case PickerView2
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,9 +31,113 @@ class ViewController6: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func genderAction(sender: UITextField) {
+        let pickerView = UIPickerView()
+        pickerView.tag = PickerViewTag.PickerView1.rawValue
+        pickerView.delegate = self
+        gender.inputView = pickerView
+        let toolBar = UIToolbar(frame: CGRectMake(0, self.view.frame.size.height/6, self.view.frame.size.width, 40.0))
+        toolBar.layer.position = CGPoint(x: self.view.frame.size.width/2, y: self.view.frame.size.height-20.0)
+        toolBar.barStyle = UIBarStyle.BlackTranslucent
+        toolBar.tintColor = UIColor.whiteColor()
+        toolBar.backgroundColor = UIColor.blackColor()
+        let doneButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.Done, target: self, action: "doneGenderPressed:")
+        let flexSpace = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.FlexibleSpace, target: self, action: nil)
+        let label = UILabel(frame: CGRect(x: 0, y: 0, width: self.view.frame.size.width / 3, height: self.view.frame.size.height))
+        label.backgroundColor = UIColor.clearColor()
+        label.textColor = UIColor.whiteColor()
+        label.text = "Gender"
+        label.textAlignment = NSTextAlignment.Center
+        let textBtn = UIBarButtonItem(customView: label)
+        toolBar.setItems([textBtn,flexSpace,doneButton], animated: true)
+        gender.inputAccessoryView = toolBar
+        gender.text = genderOptions[0]
+    }
+    
+    @IBAction func birthControlAction(sender: UITextField) {
+        let pickerView = UIPickerView()
+        pickerView.tag = PickerViewTag.PickerView2.rawValue
+        pickerView.delegate = self
+        birthControl.inputView = pickerView
+        let toolBar = UIToolbar(frame: CGRectMake(0, self.view.frame.size.height/6, self.view.frame.size.width, 40.0))
+        toolBar.layer.position = CGPoint(x: self.view.frame.size.width/2, y: self.view.frame.size.height-20.0)
+        toolBar.barStyle = UIBarStyle.BlackTranslucent
+        toolBar.tintColor = UIColor.whiteColor()
+        toolBar.backgroundColor = UIColor.blackColor()
+        let doneButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.Done, target: self, action: "doneBirthControlPressed:")
+        let flexSpace = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.FlexibleSpace, target: self, action: nil)
+        let label = UILabel(frame: CGRect(x: 0, y: 0, width: self.view.frame.size.width / 3, height: self.view.frame.size.height))
+        label.backgroundColor = UIColor.clearColor()
+        label.textColor = UIColor.whiteColor()
+        label.text = "Birth Control"
+        label.textAlignment = NSTextAlignment.Center
+        let textBtn = UIBarButtonItem(customView: label)
+        toolBar.setItems([textBtn,flexSpace,doneButton], animated: true)
+        birthControl.inputAccessoryView = toolBar
+        birthControl.text = birthControlOptions[0]
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    func doneGenderPressed(sender: UIBarButtonItem) {
+        gender.resignFirstResponder()
+    }
+    
+    func doneBirthControlPressed(sender: UIBarButtonItem) {
+        birthControl.resignFirstResponder()
+    }
+
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if let tag = PickerViewTag(rawValue: pickerView.tag) {
+            switch tag {
+            case .PickerView1:
+                return genderOptions.count
+            case .PickerView2:
+                return birthControlOptions.count
+            default:
+                print("Unknown picker view.")
+            }
+        }
+        return genderOptions.count
+    }
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if let tag = PickerViewTag(rawValue: pickerView.tag) {
+            switch tag {
+            case .PickerView1:
+                return genderOptions[row]
+            case .PickerView2:
+                return birthControlOptions[row]
+            default:
+                print("Unknown picker view.")
+            }
+        }
+        return birthControlOptions[row]
+    }
+    
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        if let tag = PickerViewTag(rawValue: pickerView.tag) {
+            switch tag {
+            case .PickerView1:
+                gender.text = genderOptions[row]
+            case .PickerView2:
+                birthControl.text = birthControlOptions[row]
+            default:
+                print("Unknown picker view.")
+            }
+        }
     }
     
     @IBAction func dateOfLMPAction(sender: UITextField) {
@@ -41,7 +157,7 @@ class ViewController6: UIViewController {
         datePickerLMPValueChanged(datePickerView) // Set the date on start.
     }
     
-    func datePickerLMPValueChanged(sender:UIDatePicker) {
+    func datePickerLMPValueChanged(sender: UIDatePicker) {
         let dateFormatter = NSDateFormatter()
         dateFormatter.dateStyle = NSDateFormatterStyle.MediumStyle
         dateFormatter.timeStyle = NSDateFormatterStyle.NoStyle
@@ -50,9 +166,9 @@ class ViewController6: UIViewController {
         dateOfLMP.text = dateFormatter.stringFromDate(sender.date)
     }
     
-    func doneLMPButtonPressed(sender:UIButton)
-    {
+    func doneLMPButtonPressed(sender: UIButton) {
         dateOfLMP.resignFirstResponder() // To resign the inputView on clicking done.
+        // TODO dateOfLMP here
     }
     
     @IBAction func dateOfNextPeriodAction(sender: UITextField) {
@@ -72,7 +188,7 @@ class ViewController6: UIViewController {
         datePickerNextPeriodValueChanged(datePickerView) // Set the date on start.
     }
     
-    func datePickerNextPeriodValueChanged(sender:UIDatePicker) {
+    func datePickerNextPeriodValueChanged(sender: UIDatePicker) {
         let dateFormatter = NSDateFormatter()
         dateFormatter.dateStyle = NSDateFormatterStyle.MediumStyle
         dateFormatter.timeStyle = NSDateFormatterStyle.NoStyle
@@ -81,9 +197,15 @@ class ViewController6: UIViewController {
         dateOfNextPeriod.text = dateFormatter.stringFromDate(sender.date)
     }
     
-    func doneNextPeriodButtonPressed(sender:UIButton)
-    {
+    func doneNextPeriodButtonPressed(sender: UIButton) {
         dateOfNextPeriod.resignFirstResponder() // To resign the inputView on clicking done.
+        // TODO dateOfNextPeriod
+    }
+
+    @IBAction func ageAction(sender: UITextField) {
+        let str = String(age.text!)
+        print(str)
+        age.resignFirstResponder()
     }
     /*
     // MARK: - Navigation
