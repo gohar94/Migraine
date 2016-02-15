7d607b41063bb6a3214cd1062b77adc0786ab4a9
gohar94 <goharirfan94@gmail.com>
2016-02-15 13:41:20 +0500

    getting to know you UI

---
+++ Migraine/ViewController6.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController6.swift
+//  Migraine
+//
+//  Created by Gohar Irfan on 2/15/16.
+//  Copyright © 2016 Gohar Irfan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController6: UIViewController {
+
+    @IBOutlet var dateLMP: UIDatePicker!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func dateLMPAction(sender: AnyObject) {
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.dateFormat = "dd-MM-yyyy"
+        let strDate = dateFormatter.stringFromDate(dateLMP.date)
+        print(strDate)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
