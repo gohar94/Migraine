50313b63de3fc46c8898227ee18796793cfd402f
gohar94 <goharirfan94@gmail.com>
2016-02-20 21:58:16 +0500

    welcome vc file now in folder

--- Migraine/WelcomeVC.swift
+++ Migraine/WelcomeVC.swift
@@ -9,15 +9,15 @@
 import UIKit
 
 class WelcomeVC: UIViewController {
-
+    
     @IBOutlet weak var welcomeLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -42,15 +42,15 @@ class WelcomeVC: UIViewController {
         NSUserDefaults.standardUserDefaults().removePersistentDomainForName(appDomain!)
         self.performSegueWithIdentifier("goto_signin", sender: self)
     }
-
+    
     /*
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
     }
     */
-
+    
 }
